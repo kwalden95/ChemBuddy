dfa8a0c3329fc5adef2bce5f07a2154c6e9254c6
Kyle Walden <kylewalden4@gmail.com>
2018-10-19 19:20:14 -0500

    commit

--- ChemBuddy/App.xaml.cs
+++ ChemBuddy/App.xaml.cs
@@ -15,32 +15,32 @@ namespace ChemBuddy
     /// </summary>
     public partial class App : Application
     {
-        //private const int MINIMUM_SPLASH_TIME = 3000; // Miliseconds 
-        //private const int SPLASH_FADE_TIME = 1500;     // Miliseconds 
+        private const int MINIMUM_SPLASH_TIME = 3000; // Miliseconds 
+        private const int SPLASH_FADE_TIME = 1500;     // Miliseconds 
 
-        //protected override void OnStartup(StartupEventArgs e)
-        //{
-        //    // Step 1 - Load the splash screen 
-        //    SplashScreen splash = new SplashScreen("beaker.png");
-        //    splash.Show(false, true);
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // Step 1 - Load the splash screen 
+            SplashScreen splash = new SplashScreen("beaker.png");
+            splash.Show(false, true);
 
-        //    // Step 2 - Start a stop watch 
-        //    Stopwatch timer = new Stopwatch();
-        //    timer.Start();
+            // Step 2 - Start a stop watch 
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
 
-        //    // Step 3 - Load your windows but don't show it yet 
-        //    base.OnStartup(e);
-        //    MainWindow mw = new MainWindow();
+            // Step 3 - Load your windows but don't show it yet 
+            base.OnStartup(e);
+            MainWindow mw = new MainWindow();
 
-        //    // Step 4 - Make sure that the splash screen lasts at least two seconds 
-        //    timer.Stop();
-        //    int remainingTimeToShowSplash = MINIMUM_SPLASH_TIME - (int)timer.ElapsedMilliseconds;
-        //    if (remainingTimeToShowSplash > 0)
-        //        Thread.Sleep(remainingTimeToShowSplash);
+            // Step 4 - Make sure that the splash screen lasts at least two seconds 
+            timer.Stop();
+            int remainingTimeToShowSplash = MINIMUM_SPLASH_TIME - (int)timer.ElapsedMilliseconds;
+            if (remainingTimeToShowSplash > 0)
+                Thread.Sleep(remainingTimeToShowSplash);
 
-        //    // Step 5 - show the page 
-        //    splash.Close(TimeSpan.FromMilliseconds(SPLASH_FADE_TIME));
-        //    mw.Show();
-        //}
+            // Step 5 - show the page 
+            splash.Close(TimeSpan.FromMilliseconds(SPLASH_FADE_TIME));
+            mw.Show();
+        }
     }
 }
--- ChemBuddy/MainWindow.xaml.cs
+++ ChemBuddy/MainWindow.xaml.cs
@@ -59,5 +59,435 @@ namespace ChemBuddy
         {
 
         }
+
+        private void BoronButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CarbonButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NitrogenButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OxygenButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FluorineButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NeonButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SodiumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MagnesiumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AluminumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SiliconButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PhosphorusButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SulfurButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ChlorineButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ArgonButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PotassiumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CalciumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TitaniumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void VanadiumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ChromiumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ManganeseButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ScandiumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void IronButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CobaltButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NickelButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CopperButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ZincButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GalliumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void StrontiumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GermaniumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ArsenicButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BromineButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void KryptonButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RubidiumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void YttriumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ZirconiumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NiobiumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MolybdenurButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MolybdenumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TechnetiumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RutheniumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RhodiumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PalladiumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SilverButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CadmiumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void IndiumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TinButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AntimonyButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TelluriumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void IodineButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void XenonButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CaesiumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BariumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void HafniumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TantalumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TungstenButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RheniumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OsmiumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void IridiumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PlatinumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MercuryButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ThalliumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LeadButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BismuthButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PoloniumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AstatineButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RadonButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FranciumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RadiumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Num89_103Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RutherfordiumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DubniumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SeaborgiumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BohriumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void HassiunButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MeitneriumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DarmstadtiumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RoentgeniumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CoperniciumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NihoniumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FleroviumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MoscoviumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LivermoriumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TennessineButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OganessonButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SeleniumButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GoldButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
